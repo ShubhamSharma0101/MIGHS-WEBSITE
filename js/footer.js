e474642c09b3e2b2a6bd0f2316ac35b965373970
ShubhamSharma0101 <shubhamsharma12532@gmail.com>
2025-10-26 23:34:28 +0530

    ../ remove

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 var footercontainer = document.getElementById("footer-container");
 var footerRequest = new XMLHttpRequest();
-footerRequest.open("GET", "../footer.html", true);
+footerRequest.open("GET", "footer.html", true);
 footerRequest.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     footercontainer.innerHTML = this.responseText;
--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 var navbarContainer = document.getElementById("navbar-container");
 var navbarRequest = new XMLHttpRequest();
-navbarRequest.open("GET", "../navbar.html", true);
+navbarRequest.open("GET", "navbar.html", true);
 navbarRequest.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     navbarContainer.innerHTML = this.responseText;
